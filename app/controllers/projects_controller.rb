c87e68dbcda29ae194580b88d5028020507030cc
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-16 22:12:46 -0500

    Integreate index action for project items

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,9 @@
 class ProjectsController < ApplicationController
+  
+  # index is typically used when you want to list out a number of items
+  def index
+    @projects_items = Project.all
+    # Project.all calls the model
+    # Any instance variables w/in the controller are accessible inside the View
+  end
 end
