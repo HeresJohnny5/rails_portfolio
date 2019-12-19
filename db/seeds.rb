d39a823e1e00e4b0219cb5389fd566e808e0de99
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-18 21:40:57 -0500

    Add new and edit link from projects page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ puts "5 skills created"
 
 9.times do |project_num|
   Project.create!(
-    title: "Project title: #{project_num}",
+    title: "Project title: #{project_num + 1}",
     subtitle: "My great service",
     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     main_image: "https://via.placeholder.com/600x400",
